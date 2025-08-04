ec81c9a8a323ae8e5b796fb7bf3a1da9b8f61287
Tomy <dead_forty@hotmail.com>
2025-08-04 12:23:22 -0600

    Fixed PostCSS config for ES Modules and verified Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Escanea archivos en src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
